24a060a7a8b7cb03ac4edde1d801c0e692d61aee
shira <shira7867@gmail.com>
2019-05-25 00:16:07 +0900

    update

--- functions/mesi-w/index.js
+++ functions/mesi-w/index.js
@@ -1,3 +1,46 @@
-exports.handler = async (event) => {
+require('dotenv').config();
+const moment = require('moment-timezone');
+moment.tz.setDefault("Asia/Tokyo");
+const Twitter = require('twitter');
+const twitterClient = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_SECRET_TOKEN,
+});
 
+
+const getMentionTimeLine = async () => {
+    return twitterClient.get('statuses/mentions_timeline', {
+        count: 20,
+        include_entities: false,
+    });
 }
+
+async function main(event) {
+    const lastRun = moment(event.time).add(-10, 'm').unix();
+    const mentions = (await getMentionTimeLine())
+        .filter(e =>
+            moment(e.created_at, "ddd MMM D HH:mm:ss ZZ YYYY").unix() > lastRun
+            && e.in_reply_to_status_id_str === null)
+        .map(e => {
+            return {
+                screenName: e.user.screen_name,
+                statusId: e.id_str,
+            };
+        });
+
+    const statuses = mentions.map(e => `https://twitter.com/${e.screenName}/status/${e.statusId}`);
+
+    for (let status of statuses.reverse()) {
+        const result = await twitterClient.post('statuses/update', { status: status });
+        console.log(result.id_str);
+    }
+}
+
+
+exports.handler = async (event) => {
+    await main(event);
+}
+
+// main({ time: '2019-05-24T14:50:00Z' }).then();
